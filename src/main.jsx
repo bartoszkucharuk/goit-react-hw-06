a3bff5e0ab372162e5cb34abb2b68850ef78b9c8
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2025-01-22 05:37:45 +0100

    const { store, persistor } = persistedStoreConfig();

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import persistedStoreConfig from './redux/store.js';
+import { Provider } from 'react-redux'; // Provider to STORE
+import { PersistGate } from 'redux-persist/integration/react'; // PersistGate to PERSISTOR
+
+const { store, persistor } = persistedStoreConfig(); //use consts from store.js
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';// defaults to localStorage for web, more info https://www.npmjs.com/package/redux-persist
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from 'redux-persist';
+import { contactsReducer } from "./contactSlice";
+import { filtersReducer } from "./filterSlice";
+
+
+//reducers config
+const rootReducer = combineReducers({
+    contacts: contactsReducer,
+    filters: filtersReducer,
+});
+
+//redux-persist config
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//function export to main.jsx (store, persistor)
+const persistedStoreConfig = () => {
+    const store = configureStore({
+        reducer: persistedReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware({
+                serializableCheck: {
+                    ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+                },
+            }),
+    });
+
+    const persistor = persistStore(store);
+    return { store, persistor };
+}
+
+export default persistedStoreConfig;
