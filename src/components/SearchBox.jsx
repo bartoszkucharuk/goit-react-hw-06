0f3abb3a82a626f0f2e4e78dde2757409f9749a8
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2025-01-25 15:59:04 +0100

    almost finished but CSS

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import { useDispatch } from "react-redux";
 import styles from "./SearchBox.module.css";
+import { setFilters } from "../redux/filterSlice";
 
 
-export default function SearchBox({value, handleFilterChange}) {
+export default function SearchBox() {
+  const dispatch = useDispatch();
+  const onChangeSearch = (event) => {
+    dispatch(setFilters(event.target.value));
+  };
+
   return (
     <form>
     <div>
       <label className={styles.searchBoxLabel}>
         Find contacts by name
-        <input type="text" value={value} onChange={handleFilterChange} className={styles.searchBoxInput} />
+          <input type="text"
+            onChange={onChangeSearch} className={styles.searchBoxInput} />
       </label>
     </div>
      </form>
